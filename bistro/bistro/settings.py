f07e03a0e8ef9aa6fc5200edb5fc03cf81352422
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-11-09 17:00:30 +0000

    Models and strs

--- bistro/bistro/settings.py
+++ bistro/bistro/settings.py
@@ -27,10 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.gitpod\.io$",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'bistro',
+    'menu',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'bistro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- bistro/menu/models.py
+++ bistro/menu/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+class Cuisine(models.Model):
+    title = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.title
+
+class Menu_Item(models.Model):
+
+    class Spice_Levels(models.IntegerChoices):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+    description = models.CharField(max_length=500)
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE, null=True)
+    price = models.FloatField()
+    spice_level = models.IntegerField(choices=Spice_Levels.choices)
+
+    def __str__(self):
+        return f"The item is {self.title}, the price is {self.price}, it's from {self.cuisine}, {self.category}, and its {self.description}"
